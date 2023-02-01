497159813e05d31f2a0791d54d6d4cdea52a7eb9
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2023-02-01 16:33:11 +0530

    New screen bounds detection system

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,10 +7,13 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D rb;
     public LogicScript logic;
 
+    private Vector2 screenBounds;
+
     public float flapStrength = 20;
     // Start is called before the first frame update
     void Start()
     {
+        screenBounds = new Vector2(Camera.main.aspect * Camera.main.orthographicSize,Camera.main.orthographicSize);
         rb = gameObject.GetComponent<Rigidbody2D>();
         rb.gravityScale = 3.5f;
         logic = GameObject.Find("LogicManager").GetComponent<LogicScript>();
@@ -27,6 +30,11 @@ public class BirdScript : MonoBehaviour
             rb.gravityScale = 0;
             rb.velocity = Vector2.zero;
         }
+
+        if(transform.position.y > screenBounds.y | transform.position.y < -screenBounds.y){
+            logic.gameOver();
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D other) {
         logic.gameOver();
